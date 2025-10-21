4d27fc71d255a101032d838a0a31d536fc5fab77
Brian Myers <brianmyers691@gmail.com>
2025-10-21 14:26:48 -0400

    Kafka and team project

--- 10_10_Kafka/kafka_consumer.py
+++ 10_10_Kafka/kafka_consumer.py
@@ -1,4 +1,4 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('quickstart-events')
+consumer = KafkaConsumer('sample-events', bootstrap_servers='10.1.20.213:9092')
 for msg in consumer:
     print (msg)
--- 10_10_Kafka/kafka_producer.py
+++ 10_10_Kafka/kafka_producer.py
@@ -1,13 +1,12 @@
 from kafka import KafkaProducer
 
 # Create a Kafka producer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='10.1.20.213:9092')
 
 # Send messages to the 'hello_world' topic
-for i in range(10):
-    message = f'Hello World {i}'.encode('utf-8')  # Encode the message as bytes
-    producer.send('quickstart-events', message)
-    print(f'Sent: {message}')
+message = 'œ∑´®†¥¨ˆøπåß∂ƒ©˙∆˚¬Ω≈ç√∫˜µ≤¬øß´®'.encode('utf-8')  # Encode the message as bytes
+producer.send('class-chat', value=message)
+print(f'Sent: {message}')
 
 # Close the producer
 producer.close()
